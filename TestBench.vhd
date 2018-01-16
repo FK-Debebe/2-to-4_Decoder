5bf19e2f27f39af83c5d2fb9776ea5a867058100
FK-Debebe <35236164+FK-Debebe@users.noreply.github.com>
2018-01-15 18:22:28 -0800

    Update TestBench.vhd

--- TestBench.vhd
+++ TestBench.vhd
@@ -1,18 +1,13 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
- 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --USE ieee.numeric_std.ALL;
- 
 ENTITY dec2to4_test IS
 END dec2to4_test;
- 
 ARCHITECTURE behavior OF dec2to4_test IS 
- 
-    -- Component Declaration for the Unit Under Test (UUT)
- 
-    COMPONENT dec2to4
+-- Component Declaration for the Unit Under Test (UUT)
+COMPONENT dec2to4
     PORT(
          enable : IN  std_logic;
          sw : IN  std_logic_vector(1 downto 0);
@@ -23,16 +18,13 @@ ARCHITECTURE behavior OF dec2to4_test IS
       signal test_seq : std_logic_vector(2 downto 0);
    --Outputs
       signal led : std_logic_vector(3 downto 0);
-    
 BEGIN
- 
 	-- Instantiate the Unit Under Test (UUT)
    uut: dec2to4 PORT MAP (
           enable => test_seq(2),
           sw => test_seq (1 downto 0),
           led => led
         );
-
    -- Stimulus process
    stim_proc: process
    begin		
@@ -52,7 +44,6 @@ BEGIN
          test_seq<="110"; 
          wait for 10ns; 
          test_seq<="111";
-         wait;      
-  end process stim_proc;
+         wait;    
+  end process;
 END;	
-
